36228a7f0016b4213b30d70478ec482fc5b896b4
flybyme <git@mangoraft.com>
2012-11-05 11:33:11 -0500

    Rewrite and push

--- test/testquery.js
+++ test/testquery.js
@@ -1,14 +1,36 @@
-var bt = new (require('../lib/transmission.js'))(9091, 'localhost')
-console.log(bt)
-
-// optional authorization options
-// bt.username = 'admin';
-// bt.password = 'password1';
-
-bt.on('update', function(torrents) {
-	console.log(torrents.length)
-	console.log(bt)
+var bt = new (require('../lib/transmission.js'))({
+	//port : 9091,
+	//host : 'localhost',
+	//username : 'admin',
+	//password : 'password1'
 })
-bt.start()
 
-bt.addTorrent('path.to.torrent')
+bt.add('http://cdimage.debian.org/debian-cd/6.0.6/i386/bt-cd/debian-6.0.6-i386-netinst.iso.torrent', function(err, result) {
+	if (err) {
+		throw err
+	}
+	console.log(result)
+	bt.stop('5db3a7a15a1391795a74b48c74df5d515a12c6f7', function(err) {
+		if (err) {
+			throw err
+		}
+		bt.start('5db3a7a15a1391795a74b48c74df5d515a12c6f7', function(err) {
+			if (err) {
+				throw err
+			}
+			bt.get('5db3a7a15a1391795a74b48c74df5d515a12c6f7', function(err, result) {
+				if (err) {
+					throw err
+				}
+				console.log(result)
+
+				bt.remove('5db3a7a15a1391795a74b48c74df5d515a12c6f7', true, function(err) {
+					if (err) {
+						throw err
+					}
+
+				})
+			})
+		})
+	})
+})
