24e7c65866d702ea77c11c2ce4057b80fea3e1f2
fedora <git@mangoraft.com>
2013-07-30 15:37:23 -0400

    Bump to verion 0.3.x

--- example.js
+++ example.js
@@ -5,29 +5,36 @@ var transmission = new Transmission({
 	host : '127.0.0.1'
 })
 
-transmission.on('added', function(hash, id, name) {
-	console.log('torrent added', hash, id, name)
-})
-transmission.on('removed', function(id) {
-	console.log('torrent removed id:', id)
-})
-transmission.on('stopped', function(id) {
-	console.log('torrent stopped id:', id)
-})
-transmission.on('force', function(id) {
-	console.log('torrent force start id:', id)
-})
-transmission.on('active', function(torrents) {
-	console.log('active torrent count:', torrents.length)
-})
+function getTorrent(id) {
+	transmission.get(id, function(err, result) {
+		if (err) {
+			throw err
+		}
+		console.log('bt.get returned ' + result.torrents.length + ' torrents')
+		result.torrents.forEach(function(torrent) {
+			console.log('hashString', torrent.hashString)
+		})
+		removeTorrent(id)
+	})
+}
 
-transmission.add('http://cdimage.debian.org/debian-cd/6.0.7/i386/bt-cd/debian-6.0.7-i386-netinst.iso.torrent', {
+function removeTorrent(id) {
+	transmission.remove(id, function(err) {
+		if (err) {
+			throw err
+		}
+		console.log('torrent was removed')
+	})
+}
+
+transmission.add('http://cdimage.debian.org/debian-cd/7.1.0/i386/bt-cd/debian-7.1.0-i386-netinst.iso.torrent', {
 	"download-dir" : "/home/torrents"
 }, function(err, result) {
 	if (err) {
-		throw err
+		return console.log(err)
 	}
 	var id = result.id
-	console.log(result)
-
+	console.log('Just added a new torrent.')
+	console.log('Torrent ID: ' + id)
+	getTorrent(id)
 })
