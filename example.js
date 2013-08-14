bdb5edcbd7ace6027186b46866c3bebaf9b108ea
fedora <git@mangoraft.com>
2013-08-14 18:25:23 -0400

    Make it more like a real app

--- example.js
+++ example.js
@@ -1,24 +1,53 @@
 var Transmission = require('./')
+var ProgressBar = require('progress');
 
 var transmission = new Transmission({
 	port : 9091,
 	host : '127.0.0.1'
 })
-
-function getTorrent(id) {
+function get(id, cb) {
 	transmission.get(id, function(err, result) {
 		if (err) {
 			throw err
 		}
-		console.log('bt.get returned ' + result.torrents.length + ' torrents')
-		result.torrents.forEach(function(torrent) {
-			console.log('hashString', torrent.hashString)
-		})
-		removeTorrent(id)
+		cb(null, result.torrents[0])
 	})
 }
 
-function removeTorrent(id) {
+function watch(id) {
+	get(id, function(err, torrent) {
+		if (err) {
+			throw err
+		}
+		var downloadedEver = 0
+		var WatchBar = new ProgressBar('  downloading [:bar] :percent :etas', {
+			complete : '=',
+			incomplete : ' ',
+			width : 35,
+			total : torrent.sizeWhenDone
+		})
+
+		function tick(err, torrent) {
+			if (err) {
+				throw err
+			}
+			var downloaded = torrent.downloadedEver - downloadedEver
+			downloadedEver = torrent.downloadedEver
+			WatchBar.tick(downloaded);
+
+			if (torrent.sizeWhenDone == torrent.downloadedEver) {
+				return remove(id)
+			}
+			setTimeout(function() {
+				get(id, tick)
+			}, 1000)
+		}
+
+		get(id, tick)
+	})
+}
+
+function remove(id) {
 	transmission.remove(id, function(err) {
 		if (err) {
 			throw err
@@ -28,13 +57,11 @@ function removeTorrent(id) {
 }
 
 transmission.add('http://cdimage.debian.org/debian-cd/7.1.0/i386/bt-cd/debian-7.1.0-i386-netinst.iso.torrent', {
-	"download-dir" : "/home/torrents"
+	//options
 }, function(err, result) {
 	if (err) {
 		return console.log(err)
 	}
 	var id = result.id
-	console.log('Just added a new torrent.')
-	console.log('Torrent ID: ' + id)
-	getTorrent(id)
+	watch(id)
 })
